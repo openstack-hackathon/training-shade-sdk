9aaf7ee4b78f3fcf8b0fb45b365b57568b17cce2
Marcela Bonell <marcelabonell@gmail.com>
2016-08-13 03:00:50 -0500

    Update init scrip

--- swift/challenge/init.sh
+++ swift/challenge/init.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
-sudo apt-get install git
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert  git -y
 
 sudo git clone https://github.com/openstack-hackathon/training-shade-sdk.git
 sudo cp -r training-shade-sdk/swift/challenge/my-web-pokedex/. /var/www/html/
