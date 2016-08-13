d7d5ece0dbc1b7473628055115bd63b191e27212
Marcela Bonell <marcelabonell@gmail.com>
2016-08-13 02:33:51 -0500

    Grant sudo in the init scrip

--- swift/challenge/init.sh
+++ swift/challenge/init.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 sudo apt-get install git
 
-git clone https://github.com/openstack-hackathon/training-shade-sdk.git
+sudo git clone https://github.com/openstack-hackathon/training-shade-sdk.git
 sudo cp -r training-shade-sdk/swift/challenge/my-web-pokedex/. /var/www/html/
